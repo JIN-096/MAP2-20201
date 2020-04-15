60258869d57abff441c06f37e66fe233c0e55a67
Semin <ohsemin4@gmail.com>
2020-04-15 21:17:14 +0900

    Set cocoapods podfiles

---
+++ MAP2-20201/MAP2-20201/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MAP2-20201
+//
+//  Created by Semin on 2020/04/15.
+//  Copyright © 2020 COMP420. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
