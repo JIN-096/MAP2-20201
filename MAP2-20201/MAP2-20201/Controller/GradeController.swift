b5c09a5268ea179e64af4e5ad470270113e049c2
Semin4 <ohsemin4@gmail.com>
2020-06-09 11:29:19 +0900

    성적 기본 뷰컨트롤러

---
+++ MAP2-20201/MAP2-20201/Controller/GradeController.swift
@@ -0,0 +1,9 @@
+//
+//  GradeController.swift
+//  MAP2-20201
+//
+//  Created by semin on 2020/06/02.
+//  Copyright © 2020 COMP420. All rights reserved.
+//
+
+import Foundation
---
+++ MAP2-20201/MAP2-20201/Model/GradeCell.swift
@@ -0,0 +1,17 @@
+//
+//  GradeCell.swift
+//  MAP2-20201
+//
+//  Created by semin on 2020/06/02.
+//  Copyright © 2020 COMP420. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class GradeCell : UITableViewCell{
+    static let identifier = "GradeCell"
+    
+    
+    
+}
